b2de170f659d09a3a5d93ae1457d12c4ba37591a
OmerAslani <aslaniomer21@gmail.com>
2022-10-20 12:21:42 +0200

    creating gitrep

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Sections/Header/Header";
+
+
+function App() {
+  return (
+    
+      <div className="App">
+        <Header />
+      </div>
+  
+  );
+}
+
+export default App;
---
+++ src/Sections/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './header.css';
+
+const Header = () => {
+
+    return(
+    <div className="Header-container">
+    <header className="App-header"> </header>
+    <header className="test"> </header>
+    </div>
+    )
+}
+
+
+
+export default Header;
